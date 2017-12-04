2fbfff67235ede2a0c76ebabd7a0eed88f9a1378
zengwendi <10483479@qq.com>
2017-12-04 17:33:33 +0800

    commit

---
+++ src/main/java/me/nereo/multi_image_selector/utils/MultiSelectorFileProvider.java
@@ -0,0 +1,13 @@
+package me.nereo.multi_image_selector.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Author : zhoujiulong
+ * Email : 754667445@qq.com
+ * Time : 2017/09/12
+ * 描述 : 避免三方框架使用了FileProvider
+ */
+
+public class MultiSelectorFileProvider extends FileProvider {
+}
